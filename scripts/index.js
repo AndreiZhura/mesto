37a009f5014c2681eecdffdf1f3b5dfc2235db43
AndreiZhura <zhura2296@yandex.ru>
2022-04-08 13:31:28 +0300

    like image

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__container')
+    // Находим поля формы в DOM
+const prolileName = document.getElementById('profileName');
+const profileProfession = document.getElementById('profileProfession');
+const nameInput = document.getElementById('nameInput');
+const jobInput = document.getElementById('jobInput');
+const popOpenButton = document.querySelector('.profile__button-edit');
+const popClose = document.querySelector('.popup__button');
+
+function open() {
+    popup.classList.add('popup_open');
+    nameInput.value = prolileName.textContent;
+    jobInput.value = profileProfession.textContent;
+}
+
+function close() {
+    popup.classList.remove('popup_open');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+    prolileName.textContent = nameInput.value;
+    profileProfession.textContent = jobInput.value;
+    // Вставьте новые значения с помощью textContent
+    close();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+popOpenButton.addEventListener('click', open);
+popClose.addEventListener('click', close);
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+
+
+/*Забавно что вы оставляете ссылки на материалы HTML-academy ,
+материал там и в правду хорошо изложен!*/
