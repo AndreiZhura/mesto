e41a4d166ae7a5ac59f5b2693f5e037ec266ae2d
AndreiZhura <zhura2296@yandex.ru>
2022-04-11 23:27:48 +0300

    ubuntu test

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__container');
 const popupElement = document.getElementById('popupElements')
-//кнопки вызывающие попап
+    //кнопки вызывающие попап
 const popupOpenButtonProfile = document.querySelector('.profile__button-edit');
 const popupOpenButtonElements = document.querySelector('.profile__button-add');
-    // Находим поля формы в DOM
+// Находим поля формы в DOM
 const prolileName = document.getElementById('profileName');
 const profileProfession = document.getElementById('profileProfession');
 const nameInput = document.getElementById('nameInput');
@@ -28,11 +28,13 @@ function openProfilePopup() {
     nameInput.value = prolileName.textContent;
     jobInput.value = profileProfession.textContent;
 }
+
 function closeProfilePopup() {
     popup.classList.add('popup_profile');
 }
-function openElementsPopup(){
-  popupElement.classList.remove('popup_elements');
+
+function openElementsPopup() {
+    popupElement.classList.remove('popup_elements');
 }
 
 function formSubmitHandler(evt) {
@@ -58,33 +60,3 @@ popClose.addEventListener('click', closeProfilePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-
-
-/*Забавно что вы оставляете ссылки на материалы HTML-academy ,
-материал там и в правду хорошо изложен!*/
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
