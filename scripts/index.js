4508d7f5fd06660dd015dc9996e147449a9787c1
AndreiZhura <zhura2296@yandex.ru>
2022-04-14 21:45:14 +0300

    5 sprint first

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // спринт 4
 // попап провиля
-let formElement = document.getElementById('popupContainer')
+let formElementPopupProfile = document.getElementById('popupContainer')
 const popupProfile = document.getElementById('popupProfile');
 const popOpenProfile = document.getElementById('popOpenProfile');
 const closeButtonProfile = document.getElementById('closeButtonProfile');
@@ -36,7 +36,83 @@ function formSubmitHandler(evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElementPopupProfile.addEventListener('submit', formSubmitHandler);
 
 popOpenProfile.addEventListener('click', functionPopupOpenrofile);
 closeButtonProfile.addEventListener('click', functionPopupCloseProfile);
+
+//спринт 5
+const popOpenElements = document.getElementById('popOpenElements');
+const closeButtonElements = document.getElementById('closeButtonElements');
+const popupElements = document.getElementById('popupElements');
+const titleImput = document.getElementById('titleImput');
+const linkInput = document.getElementById('linkInput');
+
+function functionPopOpenElements() {
+    popupElements.classList.remove('popup_elements');
+}
+
+function functionPopCloseElements() {
+    popupElements.classList.add('popup_elements');
+}
+
+popOpenElements.addEventListener('click', functionPopOpenElements);
+closeButtonElements.addEventListener('click', functionPopCloseElements);
+
+
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+const placesContainer = document.querySelector(".elements");
+const placeTemplate = document.querySelector(".template").content;
+
+const placeInfo = initialCards.map(function(item) {
+    return {
+        name: item.name,
+        link: item.link
+    };
+});
+
+function render() {
+    placeInfo.forEach(renderCard);
+}
+
+function renderCard({ name, link }) {
+    const placeElement = placeTemplate.querySelector(".element").cloneNode(true);
+    placeElement.querySelector(".element__title").textContent = name;
+    placeElement.querySelector(".element__rectangle").src = link;
+
+    placesContainer.prepend(placeElement);
+}
+
+function handelAddImage() {
+    const inputTitleValue = document.getElementById('titleImput').value;
+    const elementTitleValue = renderCard({ name: inputTitleValue });
+    placesContainer.prepend(elementTitleValue);
+
+}
+render();
