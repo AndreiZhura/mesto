8faf4c7209096261e454a7efbe35e6cc0f6d38f3
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-06-08 18:18:52 +0300

    доделываю node

--- practicum/webpack.config.js
+++ practicum/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
+
 module.exports = {
     entry: './src/scripts/index.js',
     output: {
@@ -12,7 +16,12 @@ module.exports = {
             use: "babel-loader",
             exclude: "/node_modules"
         }]
-    }
-
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html' // путь к файлу index.html
+        }),
+        new CleanWebpackPlugin(),
+    ]
 
 }
