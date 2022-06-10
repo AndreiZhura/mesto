70cdc9fac24ed703c4f3cfb1090483add0bc491f
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-06-10 12:25:29 +0300

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.export = {
-    entry: './src/scripts/index.js',
+module.exports = {
+    entry: { main: './src/scripts/index.js' },
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: ''
+    },
+    mode: 'development',
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+        open: true
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ]
+};
