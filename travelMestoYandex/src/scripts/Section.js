373ed6eb31dd258d00fa966589b10f3dc723b8f3
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-06-20 02:48:44 +0300

    1

---
+++ travelMestoYandex/src/scripts/Section.js
@@ -0,0 +1,23 @@
+export default class Section {
+    constructor({ items, renderer }, containerSelector) {
+            this._items = items;
+            this._renderer = renderer;
+            // селектор класса div куда мы передаем карточки
+            this._containerSelector = document.querySelector(containerSelector);
+
+        }
+        // рендерим данные
+    rendererValue() {
+            //передаем массив в items -> перебираем его форечем и передаем в функцию рендер
+            this._items.forEach(item => {
+                this._renderer(item)
+            });
+        }
+        // добавляем данные что бы отрисовать их на странице
+    addItem(element) {
+        //Метод prepend позволяет вставить 
+        //в начало какого-либо элемента другой элемент.
+        //append в конец
+        this._containerSelector.prepend(element)
+    }
+}
---
+++ travelMestoYandex/src/scripts/index.js
@@ -0,0 +1,209 @@
+import Card from "./Card.js";
+import Popup from "./Popup.js";
+import Section from "./Section.js";
+import PopupWithForm from "./PopupWithForm.js";
+import PopupWithImage from "./PopupWithImage.js";
+import UserInfo from './UserInfo.js'
+import FormValidator from './FormValidator.js'
+import '../pages/index.css'
+
+
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+// константы ***************************************************************************************************************
+// секция попапа профиля
+const popupProfile = document.querySelector('#popupProfile');
+// кнопка открытия попапа профиля
+const popupProfileOpenButton = document.querySelector('#popOpenProfile');
+//кнопка закрытия попапа профиля
+const popupProfileCloseButton = document.querySelector('#closeButtonProfile');
+// экспортируем иконку закрытия попапа
+export const popupClose = document.querySelector('.popup__button')
+    // имя профиля
+const profileName = document.querySelector('#profileName');
+//профессия профиля
+const profileProfession = document.querySelector('#profileProfession');
+// ввод имени профиля
+const nameInput = document.querySelector('#name-input');
+// профессия имени профиля
+const jobInput = document.querySelector('#job-input');
+// контайнер попапа профиля
+const formPopupProfile = document.querySelector('#popupContainerProfile');
+// попап создания карточки
+const cardPopup = document.querySelector('#popupElements');
+// кнопка открытия попапа карточки
+const cardPopupOpenButton = document.querySelector('#popOpenElements');
+//кнопка закрытия попапа карточки
+const cardPopupCloseButton = document.querySelector('#closeButtonElement');
+// форма контэйнера попапа карточки
+const formCardPopup = document.querySelector('#popupContainerElements');
+// попап с  картинкой и текстом
+const popupPhoto = document.querySelector('#popupPhoto');
+// кнопка его закрития
+const photoPopupButtonClose = document.querySelector('#photoPopupButtonClose');
+// div вставки карточки
+const elements = document.querySelector('.elements');
+// значение первого поля карточки попапа 
+export const inputTitleValue = document.querySelector('#title-input');
+export const inputImageValue = document.querySelector('#link-input');
+
+const popupFormProfile = document.querySelector('#popupProfile');
+const popupFormCard = document.querySelector('#popupElements');
+// валидация
+const popupProfileValid = popupFormProfile.querySelector('.popup__container')
+const popupCardValid = popupFormCard.querySelector('.popup__container');
+
+export const popupImg = document.querySelector('.popup__img')
+export const popupTitle = document.querySelector('.popup__text')
+
+export const ESC_CODE = 'Escape';
+const ENTER_CODE = 'Enter';
+
+const formValidators = {
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save',
+    inactiveButtonClass: 'popup__save_inactively',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_active'
+}
+
+
+
+
+
+
+//создаем карточку**************************************************************************************************************************
+
+const createCard = (data) => {
+    const newCard = new Card({
+        data: data,
+        handleCardClick: (name, link) => {
+            const popupWithImage = new PopupWithImage(popupPhoto)
+            popupWithImage.open(name, link)
+        }
+    }, '.template')
+    return newCard.generateCard()
+}
+
+// отрисовываем карточку****************************************************************************************************************
+const section = new Section({
+    items: initialCards,
+    renderer: (item) => {
+        section.addItem(createCard(item))
+    }
+
+}, '.elements')
+section.rendererValue()
+    /*
+    const sectionNewData = new Section({
+        items: newInputValue,
+        renderer: (item) => {
+            sectionNewData.addItem(createCard(item))
+        }
+    })*/
+
+
+// сами попапы **********************************************************************************************************************
+
+const popupClassProfile = new Popup(popupProfile)
+popupClassProfile.setEventListeners()
+
+const PopupWithFormClassProfile = new PopupWithForm({
+    selectorPopup: popupProfile,
+    buttonClose: popupProfileCloseButton,
+    popupForm: popupProfileValid,
+    submitForm: (form) => {
+        userInfo.setUserInfo(form)
+        PopupWithFormClassProfile.closePopup()
+        validatorProfile.disableButton()
+    }
+})
+PopupWithFormClassProfile.setEventListeners()
+
+
+
+
+const popupClassCard = new Popup(cardPopup)
+    // обработчики событий попапа Карточки
+popupClassCard.setEventListeners()
+
+const PopupWithFormClassCard = new PopupWithForm({
+    selectorPopup: cardPopup,
+    buttonClose: cardPopupCloseButton,
+    popupForm: popupCardValid,
+    submitForm: (item) => {
+        section.addItem(createCard(item))
+        PopupWithFormClassCard.closePopup()
+        validatorCard.disableButton()
+    }
+})
+
+PopupWithFormClassCard.setEventListeners()
+    /*
+    const PopupWithFormClassCard = new PopupWithForm({
+        selectorPopup: cardPopup,
+        buttonClose: cardPopupCloseButton,
+        popupForm: popupCardValid,
+        submitForm: () => {
+            const card = new Card({
+                data: valueInput,
+                handleCardClick: (name, link) => {
+                    const popupImage = new PopupWithImage(popupPhoto)
+                    popupImage.open(name, link)
+                }
+
+            })
+            const newCardTable = card.generateCard()
+            section.addItem(newCardTable())
+        }
+    })
+    PopupWithFormClassCard.setEventListeners()*/
+
+
+
+
+// класс UserInfo******************************************************************************************************************
+const userInfo = new UserInfo({
+        profileName: "#profileName",
+        profileProfession: "#profileProfession",
+    })
+    //Функционал кнопок открытия попапа************************************************************************************************
+
+popupProfileOpenButton.addEventListener('click', () => {
+    userInfo.getUserInfo()
+    popupClassProfile.open()
+})
+
+cardPopupOpenButton.addEventListener('click', () => {
+    popupClassCard.open()
+})
+
+const validatorProfile = new FormValidator(formValidators, popupProfileValid)
+validatorProfile.enableValidation()
+
+
+const validatorCard = new FormValidator(formValidators, popupCardValid)
+validatorCard.enableValidation()
