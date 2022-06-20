31267e82eb5b7c54a0e6c17a555f5e171003bed1
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-06-20 03:03:47 +0300

    2

--- travelMestoYandex/src/scripts/Popup.js
+++ travelMestoYandex/src/scripts/Popup.js
@@ -6,7 +6,7 @@
 - Содержит публичный метод `_setEventListeners`, который добавляет слушатель клика иконке закрытия попапа. */
 
 import { ESC_CODE } from "./index.js"
-
+/*
 export default class Popup {
 
     _selectorPopup
